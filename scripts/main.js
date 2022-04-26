43800716685ddc180afdaaf9c3a2f2301a2b6434
Victor Ochoa <vochoa.developer@gmail.com>
2022-04-25 22:00:06 -0400

    Add clear button and prompt
    Prompt allows the user to choose their desired nxn size
    of canvas on clearing.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 function initDivs(n) {
+    clearExistingDivs();
+
     const container = document.querySelector('div');
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n*n; i++) {
         const div = document.createElement('div');
 
         div.addEventListener('mouseenter', (e) => {
@@ -9,6 +11,33 @@ function initDivs(n) {
         })
         container.appendChild(div);
     }
+
+    container.style =  ` grid-template-columns: repeat(${n}, 1fr); grid-template-rows: repeat(${n}), 1fr);`
 }
 
-initDivs(16*16);
+function clearExistingDivs() {
+    const gridDivs = document.querySelectorAll('.grid-container div');
+
+    if (gridDivs) {
+        const container = document.querySelector('div');
+        gridDivs.forEach((div) => {
+            container.removeChild(div);
+        });
+    }
+}
+
+function setUpClearBtn() {
+    const clear = document.getElementById('clear');
+
+    clear.addEventListener('click', (e) => {
+        let n = prompt('How many rows and columns for your new grid?');
+        if (!(Number)(n) || (Number)(n) > 100) {
+            alert('That is not a valid number! Keep it at 100 or less...');
+            return;
+        }
+        initDivs(n);
+    });
+}
+
+initDivs(16);
+setUpClearBtn();
